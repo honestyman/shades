49b3f47f691d851df13f0b57a7ae6c08fb527dff
honestyman <leel50296@gmail.com>
2017-10-12 01:52:28 +0800

    Added initial files

---
+++ index.php
@@ -0,0 +1,163 @@
+<?php			
+					$out_value = array();
+					$box_id_number = 1; //initialize id number				
+				    $shade1 = 0; $shade2 = 0; $shade3 = 0; $shade4 = 0;
+                
+					for($i = 0; $i<4; $i++){
+						for($j = 0; $j<4; $j++){
+							
+                            
+                                $random = rand(1,4);
+                                switch($random){
+                                    case 1:
+                                        $current_value = rand(1,20);//(1,15)(1,20)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(1,25);
+                                        }
+
+                                        $current_color = 'shade1';
+                                        $shade1 += 1;
+                                        break;
+
+                                    case 2:
+                                        $current_value = rand(6,30);//(10,25)(11,30)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(1,25);
+                                        }
+
+                                        $current_color = 'shade2';
+                                        $shade2 += 1;
+                                        break;
+
+                                    case 3:
+                                        $current_value = rand(13,40);//(20,35)(21,40)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(26,50);
+                                        }
+
+                                        $current_color = 'shade3';
+                                        $shade3 += 1;
+                                        break;	
+
+
+                                    default:
+                                        $current_value = rand(27,50);//(30,50)(31,50)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(26,50);
+                                        }
+
+                                        $current_color = 'shade4';
+                                        $shade4 += 1;
+                                        break;
+                                }		
+								
+							echo "<div class='box-container'><div class='box " . $current_color . "' id='" . $box_id_number. "'><p>" . $current_value . "</p></div><div class='takip'></div></div>";
+							
+							$out_value[] = $current_value;
+							$box_id_number += 1;
+						}	
+						//echo '<br />';
+					}			
+				?>
+				
+			<script>
+			$(document).ready(function(){
+                //Hide box on click
+				$(".box, .box > p").click(function(){
+					$(this).animate({
+						"opacity": "0"
+					}, 750);
+				});
+
+                //Hide all the boxes after n seconds
+				$(function(){
+					$(".takip").delay(10000).animate({
+						"opacity": "1",
+						"top": "0"
+					}, 500);
+				});
+
+                //Timer
+				$(function(){
+					var counter = 10;
+					var interval = setInterval(function(){
+
+						counter -= 1;
+						$( "span.secs" ).html(counter);
+
+						if (counter === 0){	
+							clearInterval(interval);
+							$(".reminder").fadeOut(500);
+						}
+					}, 1000);					
+				});
+
+				var ans1;
+				var ans2;
+				var ans3;
+				var ans4;
+				
+                //Find the shortest number for each shade
+				$(function(){
+					var path1 = [];
+					$(".shade1").each(function(){ 	
+						$val = $(this).children().text();
+						path1.push(parseInt($val, 10));
+					});	
+					
+					path1.sort(function(a, b){return a-b});
+					ans1 = path1[0];
+					
+					var path2 = [];
+					$(".shade2").each(function(){ 	
+						$val = $(this).children().text();
+						path2.push(parseInt($val, 10));                      
+                        
+					});	
+					
+					path2.sort(function(a, b){return a-b});					
+					ans2 = path2[0];
+                    var i = 0;
+                    while(ans2 <= ans1){
+                        ans2 = path2[i];
+                        i+=1;
+                    }
+                    
+					var path3 = [];
+					$(".shade3").each(function(){ 	
+						$val = $(this).children().text();
+						path3.push(parseInt($val, 10));
+					});	
+					
+					path3.sort(function(a, b){return a-b});					
+					ans3 = path3[0];
+                    var j = 0;
+                    while(ans3 <= ans2){
+                        ans3 = path3[j];
+                        j+=1;
+                    }
+                    
+					var path4 = [];
+					$(".shade4").each(function(){ 	
+						$val = $(this).children().text();
+						path4.push(parseInt($val, 10));
+					});	
+					
+					path4.sort(function(a, b){return a-b});
+                    ans4 = path4[0];
+                    var k = 0;
+                    while(ans4 <= ans3){
+                        ans4 = path4[k];
+                        k+=1;
+                    }
+                    
+					$( ".path1" ).html(ans1);
+					$( ".path2" ).html(ans2);
+					$( ".path3" ).html(ans3);
+					$( ".path4" ).html(ans4);
+					
+				});
+				
+				
+			});
+		</script>
---
+++ shades.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Play Shades</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+    <link rel="stylesheet" href="scss/shades.css.min">
+</head>
+
+<body>
+    <div class="wrap">
+        <div class="container check-layout">
+            <div class="content-wrap check-layout">
+
+            </div>
+        </div>
+    </div>
+		
+
+    
+    <script src="js/prefixfree.min.js"></script>
+</body>
+</html>
