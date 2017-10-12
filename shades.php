6be5cdba176ec9fa9b6675919441f83f1b9933df
honestyman <leel50296@gmail.com>
2017-10-12 19:35:31 +0800

    Redo box layout

--- shades.php
+++ shades.php
@@ -12,14 +12,73 @@
 <body>
     <div class="wrap">
         <div class="container check-layout">
-            <div class="content-wrap check-layout">
+            <div class="card-holder check-layout">
+                <?php			
+					$out_value = array();
+					$box_id_number = 1; //initialize id number				
+				    $shade1 = 0; $shade2 = 0; $shade3 = 0; $shade4 = 0;
+                
+					for($i = 0; $i<4; $i++){
+						for($j = 0; $j<4; $j++){
+							
+                            
+                                $random = rand(1,4);
+                                switch($random){
+                                    case 1:
+                                        $current_value = rand(1,20);//(1,15)(1,20)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(1,25);
+                                        }
 
+                                        $current_color = 'shade1';
+                                        $shade1 += 1;
+                                        break;
+
+                                    case 2:
+                                        $current_value = rand(6,30);//(10,25)(11,30)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(1,25);
+                                        }
+
+                                        $current_color = 'shade2';
+                                        $shade2 += 1;
+                                        break;
+
+                                    case 3:
+                                        $current_value = rand(13,40);//(20,35)(21,40)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(26,50);
+                                        }
+
+                                        $current_color = 'shade3';
+                                        $shade3 += 1;
+                                        break;	
+
+
+                                    default:
+                                        $current_value = rand(27,50);//(30,50)(31,50)
+                                        while(in_array($current_value,$out_value)){
+                                            $current_value = rand(26,50);
+                                        }
+
+                                        $current_color = 'shade4';
+                                        $shade4 += 1;
+                                        break;
+                                }		
+								
+							echo "<div class='box-container'><div class='box " . $current_color . "' id='" . $box_id_number. "'><p>" . $current_value . "</p></div><div class='takip'></div></div>";
+							
+							$out_value[] = $current_value;
+							$box_id_number += 1;
+						}	
+						//echo '<br />';
+					}			
+				?>
             </div>
         </div>
-    </div>
-		
-
+    </div>    
     
     <script src="js/prefixfree.min.js"></script>
+    
 </body>
 </html>
