6d9e6dac2cffda0a6a08dd2fcc4eeb60a17f982e
honestyman <leel50296@gmail.com>
2017-10-23 13:41:00 +0800

    Redo box layout

--- shades.php
+++ shades.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-    <script src="js/jquery.min.js"></script>
+    <title>Shades | Play</title>
+    
+    <meta name="description" content="Description of the page less than 150 characters">
+    <link rel="icon" type="image/x-icon" href="http://localhost/shades/favicon.ico">
+    
+    
     <link rel="stylesheet" href="scss/shades.min.css">
-    
-    <title>Play Shades</title>
-    
+    <script src="js/jquery.min.js"></script>
 </head>
 
 <body>
