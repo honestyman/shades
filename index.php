df5fdb386b88390d5ba12521ec76cf4d9e430a64
honestyman <leel50296@gmail.com>
2017-10-23 16:38:54 +0800

    Create homepage layout

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
-<!DOCTYPE html>
-<html lang="en">
-<head>    
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="scss/shades.css.min">
-    
-    <title>Shades | Game</title>
-    
-</head>
+<?php
+    require_once('config.php');
+    include('includes/header.php');
+?>
 
-<body>
     <div class="wrap">
         <div class="container">
             This is the homepage asdfasdf
         </div>
     </div>
-    <script src="js/prefixfree.min.js"></script>
-</body>
-</html>
+
+<?php
+    include('includes/footer.php');
+?>
