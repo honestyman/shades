20e95d9c05daadd24cb2a9b26afbd5b2e877fa64
honestyman <leel50296@gmail.com>
2017-10-23 16:40:29 +0800

    Added config file

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/*
+    This file consists variables and functions used for he SHADES website. 
+    Date Created: October 21, 2017
+    Author: rhanmiano
+*/
+
+//As webpage opens start session
+session_start();
+
+//Dynamically set title
+function current_page(){
+    $page_title = "";
+    $url = $_SERVER["SCRIPT_NAME"];
+    $break = Explode('/', $url);
+    $file = $break[count($break) - 1];
+    
+    if($file == 'index.php')
+        $page_title = "Shades | Home";
+    else if($file == 'shades.php')
+        $page_title = "Shades | Play";
+    
+    return $page_title;
+} 
+?>
