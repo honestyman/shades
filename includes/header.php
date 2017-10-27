f8e67606d27469f24b3ab7da8d82cbc80c2dc5fd
honestyman <leel50296@gmail.com>
2017-10-27 15:35:00 +0800

    Finding absolute path for linking

--- includes/header.php
+++ includes/header.php
@@ -8,8 +8,8 @@
     <title><?php echo currentPage(); ?></title>
     <meta name="description" content="Description of the page less than 150 characters">
     
-    <link rel="icon" type="image/x-icon" href="http://localhost/shades/favicon.ico">    
-    <link rel="stylesheet" href="scss/shades.min.css">
+    <link rel="icon" type="image/x-icon" href="/shades/favicon.ico">    
+    <link rel="stylesheet" href="/shades/scss/shades.min.css">
     <script src="js/jquery.min.js"></script>
 </head>
 <body>
