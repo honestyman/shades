5a3407f9da5d03f58b9d3ac0f961b385709a68db
honestyman <leel50296@gmail.com>
2017-10-27 15:30:32 +0800

    Added Error 403 and 404 Pages

---
+++ 403.php
@@ -0,0 +1,20 @@
+<?php
+    require('config.php');
+    include('includes/header.php');
+?>
+
+<div class="wrap">
+    <div class="container">
+       <div class="col-12" id="tbl">
+          <div class="valign-mid error-page">
+              <div class="title"><p>ERROR 403</p></div>
+              <a href="<?php echo SITE_URL; ?>"><img src="/shades/images/shades-logo-w80.png" alt="Shades Logo" /></a>
+              <p class="def-text">You cannot access this file or directory</p>
+              <a href="<?php echo SITE_URL; ?>" class="modal-btn">Please get me back to home</a>
+          </div>
+       </div>
+    </div>
+</div>
+<?php
+    include('includes/footer.php');
+?>
---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php
+    require('config.php');
+    include('includes/header.php');
+?>
+
+<div class="wrap">
+    <div class="container">
+       <div class="col-12" id="tbl">
+          <div class="valign-mid error-page">
+              <div class="title"><p>ERROR 404</p></div>
+              <a href="<?php echo SITE_URL; ?>"><img src="/shades/images/shades-logo-w80.png" alt="Shades Logo" /></a>
+              <p class="def-text">Ooops! It seems that you're lost or what?</p>
+              <a href="<?php echo SITE_URL; ?>" class="modal-btn">Please get me back to home</a>
+          </div>
+       </div>
+    </div>
+</div>
+<?php
+    include('includes/footer.php');
+?>
