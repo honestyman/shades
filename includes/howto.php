222d04beeba460c2e9f43f214ff1d434e0545a28
honestyman <leel50296@gmail.com>
2017-11-04 22:53:19 +0800

    Created How to Modal

---
+++ includes/howto.php
@@ -0,0 +1,46 @@
+<div class="modal-wrap how-to hidden">
+    <span class="modal-close"></span>
+    <div class="modal-container modal-how-to">
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/shade-how-to.png" alt="Shades Sample Card">
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="modal-head">
+                            <h1>how to play</h1>
+                        </div>
+                            <p>step 1: In the grid, notice that there are four different shades of blue. Sequence them in your mind from lightest to darkest</p>
+                            <ul class="shade-guide clearfix">
+                                <li class="shade1"></li>
+                                <li class="shade2"></li>
+                                <li class="shade3"></li>
+                                <li class="shade4"></li>
+                            </ul>
+                            <p>step 2: Memorize the smallest number in each shade, from lightest to darkest. Like so:</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="images/shades-ans.png" alt="Shades Sample Card 2">
+                        </div>
+                        <div class="col-6">
+                            <p>So the proper sequence for this example is:</p>
+                            <ul class="shade-guide clearfix">
+                                <li class="shade1"><p>7</p></li>
+                                <li class="shade2"><p>3</p></li>
+                                <li class="shade3"><p>1</p></li>
+                                <li class="shade4"><p>11</p></li>
+                            </ul>
+                            <p style="clear: left;">You have 15 seconds to memorize the values, the grid's shades and number values will be covered after. You can then click each box for the proper sequence.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <p>step 3: Note that if you have misclicked boxes that are not in the proper sequence, you'll get a wrong answer. You can still complete the level by finding the remaining values in the sequence.</p>
+                        <p>step 4: You have three lives. Memorize and complete the sequence then proceed to the next level! Goodluck and have fun!.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
